d048070518eeffe1aa0c4eb40d6301f6d61e150c
digiwayakademi <destek@digiway.com.tr>
2024-07-29 23:33:53 +0300

    29-07-2024-customer-app-ders-sonu
    DIGIWAY Akademi Haftaiçi Yazılım Grubu React dersleri

---
+++ customer-app/src/components/CustomerItem/CustomerItem.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+
+const DeleteButton = styled.button`
+ background-color: red;
+ padding: 8px;
+ transition: opacity .5s ease;
+ 
+ &:hover{
+  opacity: 0.5;
+ }
+`
+
+const CustomerItem = ({ customer, handleDelete }) => {
+  return (
+    <li className="customer-item">
+      <div className="customer-info">
+        <img
+          src="https://i.pravatar.cc/300"
+          alt=""
+          className="customer-avatar"
+        />
+        <span className="customer-name">{customer.customerName}</span>
+      </div>
+      <DeleteButton onClick={() => handleDelete(customer)}>
+        <i className="bi bi-trash3"></i>
+      </DeleteButton>
+    </li>
+  );
+};
+
+export default CustomerItem;
